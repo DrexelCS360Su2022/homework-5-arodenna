4ac42d1e657572dbf2614f68cff037d9e4e9c96c
arodenna <87154997+arodenna@users.noreply.github.com>
2022-08-04 18:58:26 -0400

    Finished HW 5

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -8,48 +8,92 @@ import Set
 -- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
 
 isThisWorking :: String
-isThisWorking = "Yes"
+isThisWorking = "Finally"
 
 --
 -- Problem 1
 --
 
+-- 1.1
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
+lastDigit x = x `rem` 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit x = x `quot` 10
 
+-- 1.2
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits x 
+    | x <= 0 = []
+    | otherwise = toDigits (dropLastDigit x) ++ [lastDigit x]
 
+-- 1.3
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther x = listReverser (deoHelper (listReverser x))
 
+
+deoHelper :: [Integer] -> [Integer]
+deoHelper x 
+    | x == [] = []
+    | length x == 1 = x
+    | length x == 2 = [(head x)] ++ [(2*(head (tail x)))]
+    | otherwise = [(head x)] ++ [(2*(head (tail x)))] ++ deoHelper (tail (tail x))
+
+listReverser :: [Integer] -> [Integer]
+listReverser myList
+    | myList == [] = []
+    | otherwise = listReverser (tail myList) ++ [(head myList)]
+
+-- 1.4
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits myList 
+    | myList == [] = 0
+    | (head myList) >= 10 = (sumDigits (toDigits (head myList))) + (sumDigits (tail myList))
+    | otherwise =  (head myList) + (sumDigits (tail myList))
 
+-- 1.5
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
+validate myList 
+    | sumDigits(doubleEveryOther(toDigits(myList))) `rem` 10 == 0 = True
+    | otherwise = False
 
 --
 -- Problem 2
 --
 
+-- 2.1
 pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow f n x
+    | n == 0 = x
+    | otherwise = pow f (n - 1) (f x)
 
+-- 2.2
 g :: Integer -> Integer
-g = error "g not yet defined"
+g x 
+    | x == 0 = 0
+    | otherwise = x - (pow g 2 (x - 1))
 
 h :: Integer -> Integer
-h = error "h not yet defined"
+h x 
+    | x == 0 = 0
+    | otherwise = x - (pow h 3 (x - 1))
 
+-- 2.3
 d :: Int -> Integer -> Integer
-d = error "d not yet defined"
+d i x 
+    | x == 0 = 0 
+    | otherwise = x - (pow (d i) i (x-1))
 
 --
 -- Problem 3
 --
 
-powerSet = error "powerSet not yet defined"
+powerSet :: Ord a => Set a -> Set(Set a)
+powerSet mySet 
+    | isEmpty mySet = insert mySet empty --If mySet is empty, add empty set
+    | otherwise = let (myHead, myTail) = split mySet in (powerSet myTail) `union` (mapSet (insert myHead) (powerSet myTail))
+-- Otherwise, split the set into head and tail and union the following
+    -- the result of calling powerset on the rest/tail of the set
+    -- all combinations of the head of the set with the tail of the set
+            -- Head + Tail (size n); Head + Each Combination of Tail - 1 Element (size n-1); ... Head (size n - (n-1))
+            -- aka append head to the powerset of the tail
--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # CS 360 Homework 3
 
-Name:  
+Name:  Dora McCafferty
 
-Drexel Username:  
+Drexel Username:  arodenna
 
 Put anything else you want us to know here.
+
+Problem 1 took a bit to get used to which functions required which types, but overall wasn't hard. The hardest part was that Docker doesn't work, so all my code testing was on https://replit.com/languages/haskell first. The second question went quickly code-wise, but it took a long time to get it debugged so that tux wouldn't throw errors. Once I figured out the data structure of the third problem, it wasn't too hard. The recursion threw me for a loop for a while though.
 
 ## Problem 4: Homework statistics
 
 Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse). Please put your time estimates *on the same line* as the problem label, not on the following line.
 
-Problem 1:  
+Problem 1:  100m
 
-Problem 2:  
+Problem 2:  60m
 
-Problem 3:  
+Problem 3:  40m
